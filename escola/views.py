84e42e5d5185b47377de10031af3526b16c262c5
Kemuel Kesley <kemuelkesleytkd@outlook.com>
2023-12-02 22:26:28 -0300

    arquivos inclusos de teste

--- escola/views.py
+++ escola/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+
+    context = {
+        'welcome' : 'Welcome KemSoftware',
+        'begin' : 'Starting my first page By Kemuel Kesley'
+    }
+
+    return render(request, 'index.html', context)
